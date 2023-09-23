918eea3c9cf91e68d3d7758de9d6101f65f6aa44
George P Zacharia <79050784+Georgepzacharia@users.noreply.github.com>
2023-09-24 02:28:45 +0530

    Python function to predict wet/dry roads.
    Returns a list [dry_prob,wet_prob]

---
+++ inferweath.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+def inferweather(input_img):
+  #input_img(string) is the path to the image
+  model=YOLO('<path to best.pt>')
+  results=model.predict(source=input_img)
+  #0-Dry 1-Wet
+  result=[0]*2
+  weath=strs.probs.cpu().numpy().top1
+  conf=strs.probs.cpu().numpy().top1conf
+  result[weath]=conf
+  result[abs(weath-1)]=1-conf
+  return result
